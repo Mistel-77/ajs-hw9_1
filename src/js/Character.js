353ff1deb5a61caa9fc5468b095272c22d72e964
Yulia Klyueva <liya-n@list.ru>
2020-05-31 21:52:51 +0300

    ура, получилось

---
+++ src/js/Character.js
@@ -0,0 +1,10 @@
+export default class Character {
+  constructor(name) {
+    this.name = name;
+    this.level = 1;
+    this.health = 100;
+  }
+}
+
+/* const New1 = new SuperCharacter('reya');
+console.log(New1); */
